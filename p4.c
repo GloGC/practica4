63022513c6a241c4ad3389dbaf3194b837cbf0aa
GloGC <1710155@uab.cat>
2023-11-27 09:42:21 +0100

    2

---
+++ p4.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int my_head(){
+	char arxiu;
+	printf("Introdueix un arxiu: ");
+	scanf("%d", &arxiu);
+	char linies;
+	printf("Introdueix número de línies: ");
+	scanf("%d", &linies);
+
+	fd = int open(const char * &arxiu, int O_RDONLY);
+	
+	if(fd == -1){
+		printf("Error: l'arxiu no existeix, sortint del programa.);
+		exit();
+		}
+	
